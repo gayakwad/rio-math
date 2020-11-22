f91a59a89b75e25cc89b769a5f4108fc67112cf0
Abhishek Gayakwad <a.gayakwad@gmail.com>
2020-11-22 14:37:24 -0800

    Print all combinations

--- src/main/scala/rio/math/Operations.scala
+++ src/main/scala/rio/math/Operations.scala
@@ -1,5 +1,41 @@
 package rio.math
 
-class Operations {
+object Operations {
+
+  val concat: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a * 10 + b, "")
+  val sum: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a + b, " + ")
+  val diff: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a - b, " - ")
+  val multiply: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a * b, " * ")
+  val divide: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a / b, " / ")
+
+  val ops: Array[(Int, Int) => (Int, String)] = Array(concat, sum, diff, multiply, divide)
+
+
+  // 9,9,9,9
+  // 99 + (9 / 9)
+  // 9+9
+
+  def printAllCombinations(numbers: Array[Int], str: String, sum: Int): Unit = {
+
+    var x: Map[Int, List[String]] = Map()
+    print(numbers, str, sum)
+    println(x.size)
+    println(x.mkString("\n"))
+
+    def print(numbers: Array[Int], str: String, sum: Int): Unit = {
+
+      if (numbers.length > 0) {
+        for (op <- ops) {
+          val (head, tail) = (numbers.head, numbers.drop(1))
+          val (result, str1) = op(sum, head)
+          print(tail, s"$str${str1}${head})", result)
+        }
+      } else {
+        x += (sum -> (x.getOrElse(sum, List()) :+ str))
+      }
+
+    }
+  }
+
 
 }
--- src/test/scala/rio/math/ArrangementTest.scala
+++ src/test/scala/rio/math/ArrangementTest.scala
@@ -1,5 +1,7 @@
 package rio.math
 
+import Operations._
+
 class ArrangementTest extends org.scalatest.funsuite.AnyFunSuite {
 
   test("How many combinations are there for 9999") {
@@ -35,35 +37,12 @@ class ArrangementTest extends org.scalatest.funsuite.AnyFunSuite {
   }
 
   test("How many combinations are there for 9999 Improved ") {
-    val concat: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a * 10 + b, "")
-    val sum: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a + b, " + ")
-    val diff: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a - b, " - ")
-    val multiply: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a * b, " * ")
-    val divide: (Int, Int) => (Int, String) = (a: Int, b: Int) => (a / b, " / ")
 
-    val ops: Array[(Int, Int) => (Int, String)] = Array(concat, sum, diff, multiply, divide)
-
-    val numbers: Array[Int] = Array(9, 9, 9)
-    var x: Map[Int, String] = Map()
-    print(numbers, "(9", 9)
-
-    println(x.mkString("\n"))
-
-    def print(numbers: Array[Int], str: String, sum: Int): Unit = {
-
-      if (numbers.length > 0) {
-        for (op <- ops) {
-          val split = numbers.splitAt(1)
-          val tuple = op(sum, split._1(0))
-          print(split._2, s"$str${tuple._2}${split._1(0)})", tuple._1)
-        }
-      } else {
-        x += (sum -> str)
-        println(s"$str : $sum")
-      }
-
-    }
-
+    printAllCombinations(Array(9, 9, 9), "(9", 9)
+    printAllCombinations(Array(9, 99), "(9", 9)
+    printAllCombinations(Array(999), "(9", 9)
+    printAllCombinations(Array(9, 9), "(99", 99)
+    printAllCombinations(Array(9), "(999", 999)
   }
 
 }
